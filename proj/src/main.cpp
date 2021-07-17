8a30032f934f8595935d3be9072ffe6ef4a2d83a
Dominik Gausa <dominik.gausa@gmx.de>
2021-07-17 23:18:58 +0200

    Use Shell

--- proj/src/main.cpp
+++ proj/src/main.cpp
@@ -9,9 +9,45 @@
 #include <devicetree.h>
 #include <drivers/gpio.h>
 #include <sys/printk.h>
+#include <shell/shell.h>
+#include <console/console.h>
 
 #define LED0_NODE DT_NODELABEL(led_0)
 #define LED1_NODE DT_NODELABEL(led_1)
+
+static long blinkInterval = 100;
+
+static int cmd_util_hexdump(const struct shell *shell, size_t argc, char **argv)
+{
+	shell_print(shell, "argc = %d", argc);
+	for (size_t cnt = 0; cnt < argc; cnt++) {
+		shell_print(shell, "argv[%d]", cnt);
+		shell_hexdump(shell, (uint8_t*)argv[cnt], strlen(argv[cnt]));
+	}
+
+	return 0;
+}
+
+static int cmd_util_interval(const struct shell *shell, size_t argc, char **argv,
+		    void *data)
+{
+	blinkInterval = (intptr_t)data;
+
+	return 0;
+}
+
+
+SHELL_SUBCMD_DICT_SET_CREATE(sub_dict_cmds, cmd_util_interval,
+	(100, 100), (10, 10), (1000, 1000)
+);
+
+
+SHELL_STATIC_SUBCMD_SET_CREATE(sub_util,
+	SHELL_CMD(hexdump, NULL, "Hexdump", cmd_util_hexdump),
+	SHELL_CMD(interval, &sub_dict_cmds, "interval", NULL),
+	SHELL_SUBCMD_SET_END /* Array terminated. */
+);
+SHELL_CMD_REGISTER(util, &sub_util, "Util commands", NULL);
 
 void blink0(void)
 {
@@ -21,11 +57,11 @@ void blink0(void)
 	gpio_pin_configure_dt(&led1, GPIO_OUTPUT_INACTIVE);
 	while (1) {
 		printk("[Thread1]\n");
-		for(size_t loops = 0; loops < 10; loops++)
+		for(size_t loops = 0; loops < 100; loops++)
 		{
 			gpio_pin_set(led1.port, led1.pin, (int)led_is_on);
 			led_is_on = !led_is_on;
-			k_msleep(100);
+			k_msleep(blinkInterval);
 		}
 	}
 }
@@ -41,7 +77,7 @@ void main(void)
 
 	while (1) {
 		printk("[Thread0] %s\n", CONFIG_BOARD);
-		for(size_t loops = 0; loops < 10; loops++)
+		for(size_t loops = 0; loops < 100; loops++)
 		{
 			gpio_pin_set(led0.port, led0.pin, (int)led_is_on);
 			led_is_on = !led_is_on;
