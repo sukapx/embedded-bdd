69cb94a6e9b5f3b855b96d0b148cadb6c441ae2a
Dominik Gausa <dominik.gausa@gmx.de>
2021-09-01 20:36:04 +0200

    [Test] log SiL to reports/sil/

--- proj/lib/tests/common.h
+++ proj/lib/tests/common.h
@@ -10,6 +10,6 @@ std::ofstream GetLogFile() {
   auto testCName = std::string(::testing::UnitTest::GetInstance()->current_test_info()->test_case_name());
   std::replace(testName.begin(), testName.end(), '/', '_');
   std::replace(testCName.begin(), testCName.end(), '/', '_');
-  std::ofstream myFile("reports/"+ testCName + "_" + testName + ".csv");
+  std::ofstream myFile("reports/sil/"+ testCName + "_" + testName + ".csv");
   return myFile;
 }
